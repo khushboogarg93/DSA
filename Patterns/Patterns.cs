43b682400359112057c5a1dbf6d9d2a3331b8508
khushboo <khushboo.garg@blu.com>
2023-02-09 18:34:27 +0530

    basic learning

---
+++ Patterns/Patterns.cs
@@ -0,0 +1,19 @@
+using System;
+using System.IO; // defining the System package 
+
+class Program {
+  public static void Main (string[] args) {
+    // Console.WriteLine("Please enter n");
+    //  Console.WriteLine("Please enter n");            
+
+    // int n = Convert.ToInt32(Conso8le.ReadLine());
+
+    // if(true){
+    //   Console.WriteLine ("Hello World");
+    // }
+
+    int a= 5;
+    Console.WriteLine ((a++) * (a++));
+
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # DSA
+# Patterns
+# Basic Programming
